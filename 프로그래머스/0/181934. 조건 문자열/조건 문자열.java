613585dbb5e8d686a78a78dc9c955d2ef9fe7529
YeRin-Park <150512735+example-ring@users.noreply.github.com>
2024-05-06 19:37:44 +0900

    [level 0] Title: 조건 문자열, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
+        if ((ineq.equals(">") || ineq.equals("<")) && (eq.equals("=") || eq.equals("!"))) {
+            
+            if ((ineq.equals(">")) && (eq.equals("=")) && n >= m) {
+              answer = 1;
+            } else if ((ineq.equals("<")) && (eq.equals("=")) && n <= m) {
+                answer = 1;
+            } else if ((ineq.equals(">")) && (eq.equals("!")) && n > m) {
+                answer = 1;
+            } else if ((ineq.equals("<")) && (eq.equals("!")) && n < m) {
+               answer = 1;
+            }
+        }
+        return answer;
+    }
+}
